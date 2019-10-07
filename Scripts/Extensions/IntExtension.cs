ad8efab8707ad3795d5ac06e87c3d8b064c42c50
rygo6 <rygo6@me.com>
2019-10-06 19:17:04 -0700

    added some extensions

---
+++ Scripts/Extensions/IntExtension.cs
@@ -0,0 +1,22 @@
+﻿namespace GeoTetra.GTCommon
+{
+    public static class IntExtensions
+    {
+        public static int NegativeToZero(this int value)
+        {
+            if (value < 0) value = 0;
+            return value;
+        }
+
+        public static int NegativeToPositive(this int value)
+        {
+            if (value < 0)
+            {
+                value *= -1;
+                value--;
+            }
+
+            return value;
+        }
+    }
+}
