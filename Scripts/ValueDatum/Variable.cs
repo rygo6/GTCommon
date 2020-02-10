47049f74e17b1a398d15aaccaf14d975ad338d6f
rygo6 <rygo6@me.com>
2020-02-09 20:34:36 -0800

    Fixed android bugs.

--- Scripts/ValueDatum/Variable.cs
+++ Scripts/ValueDatum/Variable.cs
@@ -4,11 +4,9 @@ namespace GeoTetra.GTCommon.ScriptableObjects
 {
     public abstract class Variable<T> : ScriptableObject
     {
-#if UNITY_EDITOR
         [Multiline] 
         [SerializeField]
         private string _developerDescription;
-#endif
 
         public abstract T Value { get; set; }
     }
