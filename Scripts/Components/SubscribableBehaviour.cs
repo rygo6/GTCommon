7424c4678f6cb8793cee2a5dd817331d4c85689b
rygo6-MS <rygoodri@microsoft.com>
2020-11-16 02:09:54 -0800

    GameState turned to async ScriptableObject

--- Scripts/Components/SubscribableBehaviour.cs
+++ Scripts/Components/SubscribableBehaviour.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace GeoTetra.GTCommon.Components
 {
+    /// <summary>
+    /// Custom extension of MonoBehavior that includes extra functionality.
+    /// </summary>
     public class SubscribableBehaviour : MonoBehaviour
     {
+        public event Action<SubscribableBehaviour> Initialized;
+        
         public event Action<SubscribableBehaviour> Destroyed;
 
         public RectTransform RectTransform => transform as RectTransform;
+
+        protected async void Start()
+        {
+            await
+            Initialize();
+        }
+
+        protected async virtual Task Initialize()
+        {
+            Initialized?.Invoke(this);
+        }
         
         protected virtual void OnDestroy()
         {
