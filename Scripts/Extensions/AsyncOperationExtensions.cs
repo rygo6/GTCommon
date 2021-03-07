7c67a7d083bdcc9ee33a427290c1a87e1f8feae4
rygo6-MS <rygoodri@microsoft.com>
2021-03-07 02:07:44 -0800

    Key encoding working.

---
+++ Scripts/Extensions/AsyncOperationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace GeoTetra.GTCommon.Extensions
+{
+    public static class AsyncOperationExtensions
+    {
+        public static TaskAwaiter GetAwaiter(this AsyncOperation asyncOp)
+        {
+            TaskCompletionSource<object> tcs = new TaskCompletionSource<object>();
+            asyncOp.completed += obj => { tcs.SetResult(null); };
+            return ((Task)tcs.Task).GetAwaiter();
+        }
+    }
+}
